e3b1ae343a1e417a4861f3a45eaeec9258cbc9b6
Karl <chaipeng68@126.com>
2019-08-12 19:05:08 +0800

    父子工程 第一次提交

---
+++ ssm_mapper/src/main/java/com/baidu/mapper/AccountMapper.java
@@ -0,0 +1,12 @@
+package com.baidu.mapper;
+
+import com.baidu.domain.Account;
+
+import java.util.List;
+
+public interface AccountMapper {
+    //保存账户数据
+    void save(Account account);
+    //查询账户数据
+    List<Account> findAll();
+}
---
+++ ssm_service/src/main/java/com/baidu/service/impl/AccountServiceImpl.java
@@ -0,0 +1,25 @@
+package com.baidu.service.impl;
+
+import com.baidu.domain.Account;
+import com.baidu.mapper.AccountMapper;
+import com.baidu.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("accountService")
+public class AccountServiceImpl implements AccountService {
+    @Autowired
+    private AccountMapper accountMapper;
+
+
+    public void save(Account account) {
+        accountMapper.save(account);
+    }
+
+    public List<Account> findAll() {
+        return accountMapper.findAll();
+    }
+
+}
